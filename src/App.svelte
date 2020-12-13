8da7ad5a25b831835c3ba852c1d68ec381314e10
Diego Rojo <rojo@hey.com>
2020-12-13 19:22:56 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Tailwindcss from "./Tailwindcss.svelte";
+	export let name: string;
+</script>
+
+<Tailwindcss />
+
+<main class="text-center p-4 max-w-xs sm:max-w-none my-0 mx-auto">
+	<h1 class="uppercase text-6xl font-thin text-red-500">Hello {name}!</h1>
+	<p>
+		Visit the
+		<a
+			class="text-blue-700 hover:underline"
+			href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.svelte',
+    './public/**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
